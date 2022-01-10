8f1233ea81f1d191a797cc30248a11ae6edab694
xianianjun <xia.nianjun@mwee.cn>
2022-01-10 19:34:54 +0800

    修改index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const moment = require('moment');
 
 const time = moment().format('YYYY-MM-DD HH:mm:ss');
+
+console.log('========================================');
+console.log('==========  index.js执行       ==========');
 console.log(time);
+
+console.log('========================================');
