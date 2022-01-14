ecc30c07de2306b4401f7c3f0181c66cfc234ed6
xnj <summer19940609@gmail.com>
2022-01-14 15:00:29 +0800

    push test

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const moment = require('moment');
 
 const time = moment().format('YYYY-MM-DD HH:mm:ss');
 
+const time_08 = moment().utcOffset(8).fotmat('YYYY-MM-DD HH:mm:ss')
+
 console.log('========================================');
 console.log('==========  index.js执行       ==========');
-console.log(time);
+
+console.log('====> time的值为: ', time);
+console.log('====> time_08的值为: ', time_08);
 
 console.log('========================================');
